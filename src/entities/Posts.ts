62ed6cf97196d7515e0d21c17741732594b4db3e
Pedro <pedrohenrique010123456@gmail.com>
2026-04-22 19:20:01 -0300

    class-validator

--- src/entities/Posts.ts
+++ src/entities/Posts.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./User";
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
 
 @Entity()
 export class Post {
@@ -7,9 +8,14 @@ export class Post {
   id!: number;
 
   @Column("varchar")
+  @IsNotEmpty({ message: "Título é obrigatório" })
+  @IsString({ message: "O título deve ser uma string" })
+  @MinLength(5, { message: "O título precisa ter pelo menos 5 caracteres" })
   title!: string;
 
   @Column("text")
+  @IsNotEmpty({ message: "Conteúdo é obrigatório" })
+  @IsString({ message: "Conteúdo deve ser uma string" })
   content!: string;
 
   //um user pode ter mts posts
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Post } from "./Posts";
+import { IsNotEmpty, IsString } from "class-validator";
 
 @Entity()
 export class User {
@@ -7,9 +8,13 @@ export class User {
   id!: number;
 
   @Column("varchar")
+  @IsNotEmpty({ message: "Primeiro nome é obrigatório" })
+  @IsString({ message: "Primeiro nome precisa ser um texto" })
   firstName!: string;
 
   @Column("varchar")
+  @IsNotEmpty({ message: "Sobrenome nome é obrigatório" })
+  @IsString({ message: "Sobrenome nome precisa ser um texto" })
   lastName!: string;
 
   @Column({ type: "boolean", default: true })
